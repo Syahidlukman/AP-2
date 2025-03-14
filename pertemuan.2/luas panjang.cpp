a59f7cc7e9dfe8d9446d58a8d79d84a4507699db
Syahidlukman <syahidlukman98@gmail.com>
2025-03-14 15:34:26 +0700

    Pertemuan 2 : Penggunaan Operator

---
+++ pertemuan.2/luas panjang.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int luas,p,l;
+
+    cout << "masukkan panjang: ";
+    cin >> p;
+    cout << "masukkan lebar: ";
+    cin >> l;
+
+    luas = p * l;
+
+    cout <<"luasnya adalah "<< luas << endl;
+
+}
---
+++ pertemuan.2/operator.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    system("cls");
+    // assigment operator
+     a = 3;
+     b = 5;
+
+    // // arithmetical operator
+    // int tambah = a + b;
+    // int kurang = a - b;
+    // int kali = a * b;
+    // float bagi = a / b;
+    // int modulo = a % b;
+
+    // cout <<"hasil penjumlahan" << tambah <<endl;
+    // cout <<"hasil pengurangan" << kurang <<endl; 
+    // cout <<"hasil perkalian" << kali <<endl;  
+    // cout <<"hasil pembagian" << bagi <<endl;
+    // cout <<"hasil sisa bagi" << modulo <<endl;
+
+    //relational operator
+    // cout << (a==b) << endl;
+    // cout << (a<b) << endl;
+    // cout << (a<=b) << endl;
+    // cout << (a>b) << endl;
+    // cout << (a>=b) << endl;
+    // cout << (a!=b) << endl;
+
+    // logical operator (&&,||,!)
+    // cout << (true && true) << endl;
+    // cout << (true && false) << endl;
+    // cout << (false && true)<< endl;
+    // // cout << (false && false) << endl;
+    //  cout << (true || true) << endl;
+    //  cout << (true || false) << endl;
+    //  cout << (false || true)<< endl;
+    //  cout << (false || false) << endl;
+    // // cout << !true <<endl;
+    // cout << !false << endl;
+
+    // bitwise operator (&, |, ^, ~, >>, <<) --> biner
+    // cout << (5&7) <<endl;
+    // cout << (5|7) <<endl;
+    // cout << (5^7) <<endl;
+    // cout << (~7) <<endl;
+    // cout << (7<<2) <<endl;
+    // cout << (7>>2) <<endl;
+
+    // // shorthand
+    // a += 7; // a = a + 7;
+    // cout << a << endl;
+
+    // a -= 7; // a = a - 7;
+    // cout << a << endl;
+
+    // a *= 7; // a = a * 7;
+    // cout << a << endl;
+
+    // a /= 7; // a = a / 7;
+    // cout << a << endl;
+    
+    // post increment
+    // cout << b << endl;
+    // cout << ++b << endl;
+    // cout << a << endl;
+    // cout << ++a << endl;
+
+    // cout << b << endl;
+    // cout << b++ << endl;
+    // cout << a << endl;
+    // cout << a++ << endl;
+
+    //  cout << b << endl;
+    //  cout << --b << endl;
+    //  cout << a << endl;
+    //  cout << --a << endl;
+
+    // cout << b << endl;
+    // cout << b-- << endl;
+    // cout << a << endl;
+    // cout << a-- << endl;
+
+}
