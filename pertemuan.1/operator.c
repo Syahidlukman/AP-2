e7216b3933d24398bdd15d4b378c6aee7c998207
Syahidlukman <syahidlukman98@gmail.com>
2025-05-02 15:42:54 +0700

    pertemuan 1

---
+++ pertemuan.1/operator.c
@@ -0,0 +1,10 @@
+#include <>
+
+
+int tambah=a+b;
+int kurang=a+b;
+int main=a+b;
+float bagi=a+b;
+int modulo=a+b;
+
+cout<<
---
+++ pertemuan.1/operator.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    system ("CLS");
+
+    // Assignment Operator (memasukkan nilai ke dalam sebuah variabel)
+    a = 3;
+    b = 5;
+
+    //     // Arithmetical Operator
+    //     int tambah = a + b;
+    //     int kurang = a - b;
+    //     int kali = a * b;
+    //     float bagi = (float)a /(float) b; //type casting berguna untuk mengubah tipe data dari sebuah 
+    //     int modulo = a % b;
+
+    //   cout << "hasil penjumlahan = " << tambah << endl;
+    //   cout << "hasil pengurangan = " << kurang << endl;
+    //   cout << "hasil perkalian = " << kali << endl;
+    //   cout << "hasil pembagian = " << bagi << endl;
+    //   cout << "hasil sisa bagi = " << modulo << endl;
+    //   relational operator
+    //   cout << (a==b)<< endl;
+    //   cout << (a<B)<< endl;
+    //   cout << (a<=)<< endl;
+    //   cout << (a>)<< endl;
+    //   cout << (a>=b)<< endl;
+    //   cout << (a!=b)<< endl;
+
+    //logical operator
+    // cout << (true %% false) << endl;
+    // cout << (true %% false) << endl;
+    // cout << (true %% false) << endl;
+    // cout << (true %% false) << endl;
+    // cout << (true %% false) << endl;
+
+    // cout << (true // false) << endl;
+    // cout << (true // false) << endl;
+    // cout << (true // false) << endl;
+    // cout << (true // false) << endl;
+    // cout << (true // false) << endl;
+    // cout << (true // false) << endl;
+
+    // Bitwise operator (&, /, ^, ~, <<, >>) -->biner
+    // cout << (5&7) << endl;
+    // cout << (5/7) << endl;
+    // cout << (5^7) << endl;
+    // cout << (5~7) << endl;
+    // cout << (5<<7) << endl;
+    // cout << (5>>7) << endl;
+
+    //shorthand
+    // a +=7;  //a= a+7;
+    // cout<< a << endl;
+
+    // a -=7;  //a= a-7;
+    // cout<< a << endl;
+
+    // a *=7;  //a= a*7;
+    // cout<< a << endl;
+
+    // a /=7;  //a= a/7;
+    // cout<< a << endl;
+
+    // increment, decrement
+    // pre increment
+    // cout << a << endl;
+    // cout << ++a << endl;
+
+    // cout << b << endl;
+    // cout << ++b << endl;
+
+    //post increment
+    // cout << a << endl;
+    // cout << a++ << endl;
+    // cout << a << endl;
+
+    // cout << b << endl;
+    // cout << b++ << endl;
+    // cout << b << endl;
+
+    // pre decrement
+
+    // cout << a << endl;
+    // cout << --a << endl;
+    
+    // cout << b << endl;
+    // cout << --b << endl;
+
+    // post decrement
+    // cout << a << endl;
+    // cout << a-- << endl;
+    // cout << a << endl;
+
+    // cout << b << endl;
+    // cout << b-- << endl;
+    // cout << b << endl;
+    
+}
