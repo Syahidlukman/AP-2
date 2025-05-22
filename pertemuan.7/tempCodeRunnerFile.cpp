540bbab75c5589bac97e651181857b3de4699f14
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 23:21:52 +0700

    pertemuan 7 : pointer

--- pertemuan.7/tempCodeRunnerFile.cpp
+++ pertemuan.7/tempCodeRunnerFile.cpp
@@ -1,6 +1,6 @@
-cout << "isi variabel num "<< num[1] << endl;
-    cout << "alamat dari memori variabel num " << &num[0] << endl;
-    cout << "isi variabel pointer_num = " << pointer_num << endl;
-    cout << "isi variabel yang di tunjuk oleh pointer_num = " << *pointer_num << endl;
-    cout << "alamat memori variabel pointer_num = " << &pointer_num << endl;
-    cout << endl;
+// cout << "isi variabel num "<< num[1] << endl;
+//     cout << "alamat dari memori variabel num " << &num[0] << endl;
+//     cout << "isi variabel pointer_num = " << pointer_num << endl;
+//     cout << "isi variabel yang di tunjuk oleh pointer_num = " << *pointer_num << endl;
+//     cout << "alamat memori variabel pointer_num = " << &pointer_num << endl;
+//     cout << endl;
