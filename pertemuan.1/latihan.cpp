1d45c6b14557c246349938b6689c382343b9a4cb
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 22:07:48 +0700

    Update latihan.cpp

--- pertemuan.1/latihan.cpp
+++ pertemuan.1/latihan.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <cmath
+#include <cmath>
 using namespace std;
 int main(){
     double r, volume, luas, luas_permukaan;
-    cout << "masukkan jari jari bola: ";
-    cin >> r;
+//     cout << "masukkan jari jari bola: ";
+//     cin >> r;
 
-    volume = (4.0 / 3.0)* M_PI * pow(r, 3);
-    luas_permukaan = 4* M_PI * pow(r, 2);
+//     volume = (4.0 / 3.0)* M_PI * pow(r, 3);
+//     luas_permukaan = 4* M_PI * pow(r, 2);
 
-    cout<<"volume bola: "<<volume<<endl;
-    cout<<"luas permukaan bola: "<< luas_permukaan << endl;
+//     cout<<"volume bola: "<<volume<<endl;
+//     cout<<"luas permukaan bola: "<< luas_permukaan << endl;
 
-    return 0;
-}
+//     return 0;
+// }
