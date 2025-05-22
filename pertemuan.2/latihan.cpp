e3e9aeec5f08a9a05ce1c87089edae559ea9820d
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 22:41:00 +0700

    Update latihan.cpp

--- pertemuan.2/latihan.cpp
+++ pertemuan.2/latihan.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main() {
-    const double PI = 3.14;
-    double radius, volume, permukaan;
+//     const double PI = 3.14;
+//     double radius, volume, permukaan;
 
-    cout << "Masukkan jari-jari bola: ";
-    cin >> radius;
+//     cout << "Masukkan jari-jari bola: ";
+//     cin >> radius;
 
-    volume = (4.0 / 3.0) * PI * pow(radius, 3);
-    permukaan = 4 * PI * pow(radius, 2);
+//     volume = (4.0 / 3.0) * PI * pow(radius, 3);
+//     permukaan = 4 * PI * pow(radius, 2);
 
-    cout << "Volume bola: " << volume << endl;
-    cout << "Luas permukaan bola: " << permukaan << endl;
+//     cout << "Volume bola: " << volume << endl;
+//     cout << "Luas permukaan bola: " << permukaan << endl;
 
-    return 0;
-}
+//     return 0;
+// }
