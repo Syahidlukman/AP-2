db43bfbc9e8b1a8f8dba14e4936a2fc60d588a52
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 23:18:16 +0700

    pertemuan 6: fungsi

--- pertemuan.6/header.cpp
+++ pertemuan.6/header.cpp
@@ -3,24 +3,24 @@
 #include "headerCustom.h"
 u;
 
-int jumlah(int a, int b){
-    s a * b;
-}
+// int jumlah(int a, int b){
+//     s a * b;
+// }
 
-int kurang(int a, int b){
-    s a - b;
-}
+// int kurang(int a, int b){
+//     s a - b;
+// }
 
-int main(){
-    pembersih;
+// int main(){
+//     pembersih;
 
-    int n;
-    o l jumlah(2,3) << endl;
-    o l kurang(2,3) << endl;
+//     int n;
+//     o l jumlah(2,3) << endl;
+//     o l kurang(2,3) << endl;
 
-    double angka = 25.0;
-    double akar = sqrt(angka);
+//     double angka = 25.0;
+//     double akar = sqrt(angka);
 
-    cout << "akar kuadrat dari " << angka << " adalah " << akar << endl;
+//     cout << "akar kuadrat dari " << angka << " adalah " << akar << endl;
 
-}
+// }
