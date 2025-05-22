35e9ab7187bdcd99eea5479584df358386d7ee7e
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 22:07:10 +0700

    Update hitung bola.cpp

--- pertemuan.1/hitung bola.cpp
+++ pertemuan.1/hitung bola.cpp
@@ -4,12 +4,12 @@ int main() {
     double r, volume;
     const double PI = 3.14;
 
-    std::cout << "Masukkan jari-jari: ";
-    std::cin >> r;
+//     std::cout << "Masukkan jari-jari: ";
+//     std::cin >> r;
 
-    volume = ((float)4.0 / 3.0) * PI * (r * r * r);
+//     volume = ((float)4.0 / 3.0) * PI * (r * r * r);
 
-    std::cout << "Volume bola: " << volume << std::endl;
+//     std::cout << "Volume bola: " << volume << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
