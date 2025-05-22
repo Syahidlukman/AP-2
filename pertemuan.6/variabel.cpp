7b339ea41eb8196c3c74292b0e023305ffccc8c7
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 23:18:57 +0700

    pertemuan 6: fungsi

--- pertemuan.6/variabel.cpp
+++ pertemuan.6/variabel.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 using namespace std;
 
-string namaglobal = "ilmu";
+// string namaglobal = "ilmu";
 
-void namavariabel(){
-    string namalokal = "komputer";
+// void namavariabel(){
+//     string namalokal = "komputer";
 
-    // coba akses
-    cout << namalokal << endl;
+//     // coba akses
+//     cout << namalokal << endl;
 
     
-    // coba akses
-    cout << namaglobal << endl;
+//     // coba akses
+//     cout << namaglobal << endl;
 
 
-}
+// }
 
-int main(){
-    namavariabel();
+// int main(){
+//     namavariabel();
 
-    // coba akses
-    cout << namaglobal << endl;
+//     // coba akses
+//     cout << namaglobal << endl;
 
-    // coba akses
-     cout << namalokal << endl;
+//     // coba akses
+//      cout << namalokal << endl;
 
-}
+// }
