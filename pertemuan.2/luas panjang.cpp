b4a8ec64c60332cd38f26c0aef6a4f3ccfba277a
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 22:48:09 +0700

    pertemuan 2 :  penggunaan operator

--- pertemuan.2/luas panjang.cpp
+++ pertemuan.2/luas panjang.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main(){
 
-    int luas,p,l;
+//     int luas,p,l;
 
-    cout << "masukkan panjang: ";
-    cin >> p;
-    cout << "masukkan lebar: ";
-    cin >> l;
+//     cout << "masukkan panjang: ";
+//     cin >> p;
+//     cout << "masukkan lebar: ";
+//     cin >> l;
 
-    luas = p * l;
+//     luas = p * l;
 
-    cout <<"luasnya adalah "<< luas << endl;
+//     cout <<"luasnya adalah "<< luas << endl;
 
-}
+// }
