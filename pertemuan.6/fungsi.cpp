e8823b8b7bd1b13c8454bc19adfcd0a91653c9e9
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 23:17:44 +0700

    pertemuan 6: fungsi

--- pertemuan.6/fungsi.cpp
+++ pertemuan.6/fungsi.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
 using namespace std;
 
-// funsi tanpa nilai balikan
-void tampilkanpesan(){
-    cout << "=== SELAMAT DATANG DI AP 2 ===" << endl;
-}
+// // funsi tanpa nilai balikan
+// void tampilkanpesan(){
+//     cout << "=== SELAMAT DATANG DI AP 2 ===" << endl;
+// }
 
-// fungsi dengan nilai balikan
-int tambah (int a,int b){
-    return a + b;
-}
+// // fungsi dengan nilai balikan
+// int tambah (int a,int b){
+//     return a + b;
+// }
 
-// fungsi overload
-int kali(int a, int b){
-    return a * b;
-}
+// // fungsi overload
+// int kali(int a, int b){
+//     return a * b;
+// }
 
-double kali(double a, double b){
-    return a * b;
-}
+// double kali(double a, double b){
+//     return a * b;
+// }
 
-// fungsi rekursif
-int faktorial(int n){
-    if (n == 0 || n ==1){
-        return 1;
-    }else{
-        return n * faktorial (n-1);
-    }
-}
+// // fungsi rekursif
+// int faktorial(int n){
+//     if (n == 0 || n ==1){
+//         return 1;
+//     }else{
+//         return n * faktorial (n-1);
+//     }
+// }
 
-int main(){
-    system("CLS");
+// int main(){
+//     system("CLS");
 
-    // menggunakan fungsi tanpa nilai balikan
-    tampilkanpesan();
+//     // menggunakan fungsi tanpa nilai balikan
+//     tampilkanpesan();
     
-    int  x = 5, y = 3;
-    // menggunakan fungsi dgn nilai balikan 
-    int hasiltambah = tambah(x,y);
-    cout << "hasil penjumlahan: " << hasiltambah << endl;
+//     int  x = 5, y = 3;
+//     // menggunakan fungsi dgn nilai balikan 
+//     int hasiltambah = tambah(x,y);
+//     cout << "hasil penjumlahan: " << hasiltambah << endl;
 
-    // menggunakan fungsi overload
-    int hasilkaliint = kali(5.5, 2.0);
-    double  hasilkalidouble = kali(5.5, 2.0);
+//     // menggunakan fungsi overload
+//     int hasilkaliint = kali(5.5, 2.0);
+//     double  hasilkalidouble = kali(5.5, 2.0);
 
-    cout << "hasil perkalian (int): " << hasilkaliint << endl;
-    cout << "hasil perkalian (double): " << hasilkalidouble << endl;
+//     cout << "hasil perkalian (int): " << hasilkaliint << endl;
+//     cout << "hasil perkalian (double): " << hasilkalidouble << endl;
 
-    // menggunakan fungsi rekursif
-    int angka = 5;
-    cout << "faktorial dari " << angka << " adalah " << faktorial(angka) << endl;
-}
+//     // menggunakan fungsi rekursif
+//     int angka = 5;
+//     cout << "faktorial dari " << angka << " adalah " << faktorial(angka) << endl;
+// }
