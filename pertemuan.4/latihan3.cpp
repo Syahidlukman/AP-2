bde7cdb2aadee2c6bef35321de995426a7e7982c
Syahidlukman <syahidlukman98@gmail.com>
2025-05-22 23:00:01 +0700

    pertemuan 4 : perulangan

--- pertemuan.4/latihan3.cpp
+++ pertemuan.4/latihan3.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main() {
-    string password, sandi, expected = "";
+//     string password, sandi, expected = "";
 
-    cout << "Masukkan kata sandi : ";
-    cin >> password;
+//     cout << "Masukkan kata sandi : ";
+//     cin >> password;
 
-    cout << "Masukkan kata : ";
-    cin >> sandi;
+//     cout << "Masukkan kata : ";
+//     cin >> sandi;
 
-    // Membuat string "expected" berdasarkan pola tertentu
-    for (int i = 0; i < password.length(); i++) {
-        expected += password.substr(0, i + 1);
-    }
+//     // Membuat string "expected" berdasarkan pola tertentu
+//     for (int i = 0; i < password.length(); i++) {
+//         expected += password.substr(0, i + 1);
+//     }
 
-    // Mengecek apakah input kedua sesuai dengan pola yang diharapkan
-    if (expected == sandi) {
-        cout << "Tulisan agen benar" << endl;
-    } else {
-        cout << "Tulisan agen salah" << endl;
-    }
+//     // Mengecek apakah input kedua sesuai dengan pola yang diharapkan
+//     if (expected == sandi) {
+//         cout << "Tulisan agen benar" << endl;
+//     } else {
+//         cout << "Tulisan agen salah" << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
